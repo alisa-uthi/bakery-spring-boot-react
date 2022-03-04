dc450cf0a1a4b83de5eee8cb810e866009718fa9
alisa-uthi <alisa.uth99@gmail.com>
2022-03-04 16:38:27 +0700

    feat: add banner section in home page

--- bakery-nextjs/components/BannerSection.tsx
+++ bakery-nextjs/components/BannerSection.tsx
@@ -1,9 +1,33 @@
 import { NextComponentType } from 'next'
+import Link from 'next/link'
 
 const BannerSection: NextComponentType = () => {
   return (
-    <div className="md:mt-4">
-      <img src="/images/home-banner.jpg" alt="Home Banner" height={300} />
+    <div className="border-b border-gray-500 md:mt-4">
+      <img
+        src="/images/home-banner.jpg"
+        alt="Home Banner"
+        style={{
+          height: '600px',
+          width: '100%',
+          objectFit: 'cover',
+        }}
+        className="md:absolute md:z-0"
+      />
+      <div className="mx-10 mt-5 text-center md:absolute md:z-50 md:w-1/2 md:py-28">
+        <p className="md:text-lg md:text-white">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore
+          repellat ab ipsam quasi sit tempora adipisci
+        </p>
+        <h1 className="mt-4 text-4xl font-extrabold text-yellow-800 md:mt-10 md:text-5xl md:text-yellow-600">
+          Breads, Cakes, Pastries, and much more !!
+        </h1>
+        <Link href="/">
+          <button className="mt-5 bg-yellow-800 py-2 px-5 font-semibold text-white hover:bg-yellow-700 md:mt-10 md:bg-yellow-600 md:hover:bg-yellow-500">
+            SHOP NOW
+          </button>
+        </Link>
+      </div>
     </div>
   )
 }
--- bakery-nextjs/components/Header.tsx
+++ bakery-nextjs/components/Header.tsx
@@ -5,19 +5,19 @@ const Header: NextPage = () => {
   return (
     <div className="mt-6 md:flex">
       <h1
-        className="flex w-full justify-center border-b border-gray-300 py-3 text-4xl font-bold text-red-800
+        className="flex w-full justify-center border-b border-gray-300 py-3 text-4xl font-bold text-yellow-800
 			md:ml-20 md:flex-grow md:border-none"
       >
         Alisa Bakery
       </h1>
 
       <div className="justify-end md:mr-10 md:flex">
-        <h3 className="flex w-full cursor-pointer justify-center whitespace-nowrap border-b border-gray-300 py-3 text-2xl font-semibold hover:text-red-800 md:mr-10 md:border-none">
+        <h3 className="flex w-full cursor-pointer justify-center whitespace-nowrap border-b border-gray-300 py-3 text-2xl font-semibold hover:text-yellow-800 md:mr-10 md:border-none">
           (780) 489-4142
         </h3>
         <div className="flex w-full justify-center border-b border-gray-300 py-3 shadow-md md:w-fit md:items-center md:whitespace-nowrap md:border-none md:shadow-none">
           <ShoppingCartIcon className="h-7 w-7" />
-          <div className="h-4 w-4 rounded-full bg-red-400"></div>
+          <div className="h-4 w-4 rounded-full bg-orange-300"></div>
         </div>
       </div>
     </div>
