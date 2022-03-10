9dac6591095797d83a1143a85f5d1c85523b9e9e
alisa-uthi <alisa.uth99@gmail.com>
2022-03-10 15:07:29 +0700

    feat: add config to persist state

--- bakery-nextjs/app/store.ts
+++ bakery-nextjs/app/store.ts
@@ -1,10 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from '../slices/cartSlice'
+import storage from 'redux-persist/lib/storage'
+import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import thunk from 'redux-thunk'
+
+const reducers = combineReducers({
+  cart: cartReducer,
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
 
 export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
--- bakery-nextjs/pages/_app.tsx
+++ bakery-nextjs/pages/_app.tsx
@@ -3,13 +3,19 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import { store } from '../app/store'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+
+let persistor = persistStore(store)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <PersistGate loading={null} persistor={persistor}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </PersistGate>
     </Provider>
   )
 }
