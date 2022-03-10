d63034c132652717a799d7dc404eb3568393b708
alisa-uthi <alisa.uth99@gmail.com>
2022-03-10 22:31:30 +0700

    feat: add progress bar when route is changed

--- bakery-nextjs/pages/_app.tsx
+++ bakery-nextjs/pages/_app.tsx
@@ -5,7 +5,22 @@ import { store } from '../app/store'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
+import Router from 'next/router'
 
+import ProgressBar from '@badrap/bar-of-progress'
+
+// Progress Bar
+const progress = new ProgressBar({
+  size: 3,
+  color: '#a16207',
+  className: 'z-50',
+  delay: 80,
+})
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', progress.finish)
+Router.events.on('routeChangeError', progress.finish)
+
+// Redux Persist
 let persistor = persistStore(store)
 
 function MyApp({ Component, pageProps }: AppProps) {
